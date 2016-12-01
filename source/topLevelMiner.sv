c2b7dc6352507747a8f6855a1c2c802e8ffe5980
Mentor Graphics <mg23@ee215lnx16.ecn.purdue.edu>
2016-12-01 18:29:05 -0500

    top level update

--- source/topLevelMiner.sv
+++ source/topLevelMiner.sv
@@ -1,23 +1,26 @@
 module topLevelMiner
 (
 	input logic clk,
-	input logic n_rst,
-	
+	input logic n_rst	
 );
 
 wire validFromComparator, overflowFromNonceGen, ltFromController,
 lmFromController, resetFromController, incFromController, errFromController,
-beginComputationFromController;
+beginComputationFromController, newTargetFromED, newMsgFromED, computationCompleteFromSHA;
 
-logic [94:0][31:0] registersFromSlave;
+wire [23:0][31:0] messageFromRegisters;
+
+logic [23:0][31:0] registersFromSlave;
 wire [255:0] SHAoutfromSHABlock;
 logic [255:0] finishedSHA; 
 
-custom_slave #(
+assign messageFromRegisters = {registersFromSlave[15:3], registersFromSlave[2][31:8]};
+
+/*custom_slave #(
 	MASTER_ADDRESSWIDTH = 26 ,  	// ADDRESSWIDTH specifies how many addresses the Master can address 
 	SLAVE_ADDRESSWIDTH = 3 ,  	// ADDRESSWIDTH specifies how many addresses the slave needs to be mapped to. log(NUMREGS)
 	DATAWIDTH = 32 ,    		// DATAWIDTH specifies the data width. Default 32 bits
-	NUMREGS = 95 ,       		// Number of Internal Registers for Custom Logic
+	NUMREGS = 24 ,       		// Number of Internal Registers for Custom Logic
 	REGWIDTH = 32       		// Data Width for the Internal Registers. Default 32 bits
 ) avalonSlave
 (	
@@ -41,7 +44,7 @@ custom_slave #(
         .slave_readdata(,
 	.csr_registers(registersFromSlave)
 
-);
+);*/
 
 SHAcomputationalBlock SHABlock
 (
@@ -55,7 +58,7 @@ SHAcomputationalBlock SHABlock
 
 comparator compare
 (
-	.target(targetFromRegister),
+	.target(registersFromSlave[23:16]),
 	.msg(finishedSHA),
 	.valid(validFromComparator)
 );
@@ -64,25 +67,43 @@ controller internalController
 (
 	.clk(clk),
 	.n_rst(n_rst),
-	.newTarget,
-	.newMsg,
-	.complete,
+	.newTarget(newTargetFromED),
+	.newMsg(newMsgFromED),
+	.complete(computationCompleteFromSHA),
 	.valid(validFromComparator),
 	.overflow(overflowFromNonceGen),
 	.loadTarget(ltFromController),
 	.loadMsg(lmFromController),
 	.reset(resetFromController),
+	.beginSHA(beginComputationFromController),
 	.increment(incFromController),
 	.error(errFromController)
-	.beginComputation(beginComputationFromController)
+);
+
+risignEdgeDetect newTarget
+(
+	.clk(clk),
+	.n_rst(n_rst),
+	.currentValue(registersFromSlave[0][0]),
+	.risingEdgeDetected(newTargetFromED)
+);
+
+risignEdgeDetect newMsg
+(
+	.clk(clk),
+	.n_rst(n_rst),
+	.currentValue(registersFromSlave[0][1]),
+	.risingEdgeDetected(newMsgFromED)
 );
 
 always_ff @ (posedge clk, negedge n_rst)
 begin
 	if(!n_rst) begin
-		finishedSHA <= 0';
-	end else begin
+		finishedSHA <= '0;
+	end else if(computationCompleteFromSHA) begin
 		finishedSHA <= SHAoutfromSHABlock;
+	end else begin
+		finishedSHA <= finishedSHA;
 	end
 end
 
