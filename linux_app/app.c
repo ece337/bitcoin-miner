c63c790eb9461ba98c5995509ef1e12cca47f1ec
Austin Horning <austinseven@outlook.com>
2016-12-02 19:17:14 -0500

    Added some code for the atom

--- linux_app/app.c
+++ linux_app/app.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <memory.h>
+#include <math.h>
 
 #include "PCIE.h"
+#include "sha256.c"
 
 //MAX BUFFER FOR DMA
 #define MAXDMA 32
+
+#define BITCOIN_WORDS 19
+#define 
 
 //BASE ADDRESS FOR CONTROL REGISTER
 #define CRA 0x00000000		// This is the starting address of the Custom Slave module. This maps to the address space of the custom module in the Qsys subsystem.
@@ -17,42 +22,101 @@
 #define RWSIZE (32 / 8)
 PCIE_BAR pcie_bars[] = { PCIE_BAR0, PCIE_BAR1 , PCIE_BAR2 , PCIE_BAR3 , PCIE_BAR4 , PCIE_BAR5 };
 
-void test32( PCIE_HANDLE hPCIe, DWORD addr );
-void testDMA( PCIE_HANDLE hPCIe, DWORD addr);
+void initializePCIe();
+void operation_loop();
+void dummy_loop();
+void constructBitcoinMessage(DWORD*)
 
-int main(void)
+PCIE_HANDLE hPCIe;
+DWORD writeAddress = CRA;
+DWORD nonceAddress  = CRA + BITCOIN_WORDS;
+DWORD controlAddress = CRA + BITCOIN_WORDS + 1;
+DWORD signalAddress = CRA + BITCOIN_WORDS + 2;
+
+
+char * usage = "For dummy mode, pass the flag -d.\n";
+
+int main(argc, char ** argv)
 {
+	initializePCIe();
+
+	DWORD * data = malloc(sizeof(DWORD)*BITCOIN_WORDS);
+
+	if(argc == 1){
+		operation_loop();
+	}else if(strcmp(argv[1], "-d") == 0){
+		dummy_loop();
+	}else{
+		printf(usage);
+		exit(1);
+	}
+	return 0;
+}
+
+void initializePCIe(){
 	void *lib_handle;
-	PCIE_HANDLE hPCIe;
 
 	lib_handle = PCIE_Load();		// Dynamically Load the PCIE library
-	if (!lib_handle)
-	{
+	if (!lib_handle){
 		printf("PCIE_Load failed\n");
-		return 0;
+		exit(1);
 	}
 	hPCIe = PCIE_Open(0,0,0);		// Every device is a like a file in UNIX. Opens the PCIE device for reading/writing
 
-	if (!hPCIe)
-	{
+	if (!hPCIe){
 		printf("PCIE_Open failed\n");
-		return 0;
+		exit(1);
 	}
+}
 
-	//test CRA
-	printf("Testing the configuration registers for the Custom Slave Module\n");
-	test32(hPCIe, CRA);			// Test the Configuration Registers for reads and writes
-	printf("*******************************************************************************");
-	//test SDRAM
-	printf("************Testing the SDRAM for reads and writes through the DMA ************");
-	testDMA(hPCIe,SDRAM);			// Test the SDRAM for reads and writes
-	return 0;
+inline float fast_log(float val)
+{
+   int * const exp_ptr = reinterpret_cast <int *>(&val);
+   int x = *exp_ptr;
+   const int log_2 = ((x >> 23) & 255) - 128;
+   x &= ~(255 << 23);
+   x += 127 << 23;
+   *exp_ptr = x;
+ 
+   val = ((-1.0f/3) * val + 2) * val - 2.0f/3;
+   return ((val + log_2) * 0.69314718f);
+} 
+ 
+float difficulty(unsigned int bits){
+    static double max_body = fast_log(0x00ffff), scaland = fast_log(256);
+    return exp(max_body - fast_log(bits & 0x00ffffff) + scaland * (0x1d - ((bits & 0xff000000) >> 24)));
+}
+
+void verifySHAoutput(char * output, difficulty){
+	uchar hash[32];
+	SHA256_CTX ctx;
+	sha256_init(&ctx);
+	sha256_update(&ctx,output,BITCOIN_WORDS + 1);
+	sha256_final(&ctx,hash);
+	
+}
+
+void constructBitcoinMessage(DWORD * data){
+
+}
+
+void updateBitcoinMessage(){
+
+}
+
+void writeBitcoinMessage(DWORD * data){
+	for(int i = 0; i < BITCOIN_WORDS; i++){
+		BOOL bPass = PCIE_Write32( hPCIe, pcie_bars[0], addr + i, data[i]);
+		if (!bPass){
+			printf("test FAILED: write did not return success on setup");
+			return;
+		}
+	}
 }
 
 //Tests 16 consecutive PCIE_Write32 to addresses mapping to the Custom Slave
 
-void test32( PCIE_HANDLE hPCIe, DWORD addr )
-{
+void test32( PCIE_HANDLE hPCIe, DWORD addr ){
 	BOOL bPass;
 	DWORD testVal = 0xf;
 	DWORD readVal;
@@ -83,50 +147,6 @@ void test32( PCIE_HANDLE hPCIe, DWORD addr )
 		}
 		testVal = testVal + 1;
 		addr = addr + 4;
-	}
-	return;
-}
-
-//tests DMA write of buffer to addresses in the SDRAM 
-//
-void testDMA( PCIE_HANDLE hPCIe, DWORD addr)
-{
-	BOOL bPass;
-	DWORD testArray[MAXDMA];
-	DWORD readArray[MAXDMA];
-	
-	WORD i = 0;
-	
-	while ( i < MAXDMA )
-	{
-		testArray[i] = i  + 0x0f;
-		i++;
-	}
-
-	bPass = PCIE_DmaWrite(hPCIe, addr, testArray, MAXDMA * RWSIZE );
-	if (!bPass)
-	{
-		printf("test FAILED: write did not return success");
-		return;
-	}
-	bPass = PCIE_DmaRead(hPCIe, addr, readArray, MAXDMA * RWSIZE );
-	if (!bPass)
-	{
-		printf("test FAILED: read did not return success");
-		return;
-	}
-	i = 0;
-	while ( i < MAXDMA )
-	{
-		if (testArray[i] == readArray[i])
-		{
-			printf("Test PASSED: expected %x, received %x\n", testArray[i], readArray[i]);
-		}
-		else
-		{
-			printf("Test FAILED: expected %x, received %x\n", testArray[i], readArray[i]);
-		}
-		i++;
 	}
 	return;
 }
